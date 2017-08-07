ece87c229a85cadf43529c0e7c8e1a458609da22
Lee Borlace <lee.borlace@ignia.com.au>
2017-08-07 16:22:35 +0800

    Got initial O365 auth working.

---
+++ SharePointBot/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SharePointBot
+{
+    public static class Constants
+    {
+        public static class Commands
+        {
+            public const string LOGIN = "login";
+        }
+
+        public static class UtteranceRegexes
+        {
+        }
+
+        public static class Choices
+        {
+        }
+
+        public static class Responses
+        {
+        }
+    }
+}
--- SharePointBot/Dialogs/RootDialog.cs
+++ SharePointBot/Dialogs/RootDialog.cs
@@ -2,6 +2,13 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using BotAuth.Models;
+using System.Configuration;
+using BotAuth.Dialogs;
+using BotAuth.AADv2;
+using System.Net.Http;
+using BotAuth;
+using System.Threading;
 
 namespace SharePointBot.Dialogs
 {
@@ -17,15 +24,34 @@ namespace SharePointBot.Dialogs
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
+            var message = await result;
             var activity = await result as Activity;
+            var userToBot = activity.Text.ToLowerInvariant();
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            if (userToBot.Trim().Equals(Constants.Commands.LOGIN, StringComparison.OrdinalIgnoreCase))
+            {
+                // Initialize AuthenticationOptions and forward to AuthDialog for token
+                AuthenticationOptions options = new AuthenticationOptions()
+                {
+                    Authority = ConfigurationManager.AppSettings["aad:Authority"],
+                    ClientId = ConfigurationManager.AppSettings["aad:ClientId"],
+                    ClientSecret = ConfigurationManager.AppSettings["aad:ClientSecret"],
+                    Scopes = new string[] { "User.Read" },
+                    RedirectUrl = ConfigurationManager.AppSettings["aad:Callback"]
+                };
+                await context.Forward(new AuthDialog(new MSALAuthProvider(), options), async (IDialogContext authContext, IAwaitable<AuthResult> authResult) =>
+                {
+                    var authResultAwaited = await authResult;
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
+                    // Use token to call into service
+                    var json = await new HttpClient().GetWithAuthAsync(authResultAwaited.AccessToken, "https://graph.microsoft.com/v1.0/me");
+                    await authContext.PostAsync($"I'm a simple bot that doesn't do much, but I know your name is {json.Value<string>("displayName")} and your UPN is {json.Value<string>("userPrincipalName")}");
+                }, message, CancellationToken.None);
+            }
+            else
+            {
+                context.Wait(MessageReceivedAsync);
+            }
         }
     }
 }
